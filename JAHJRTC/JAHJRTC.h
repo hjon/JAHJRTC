f6642c8ea32850d496ec19701e7e04a7f67c9024
Jon Hjelle <hjon@andyet.net>
2015-11-16 15:35:47 -0800

    Add JAHJRTC as the entry point

---
+++ JAHJRTC/JAHJRTC.h
@@ -0,0 +1,11 @@
+//
+//  JAHJRTC.h
+//
+//  Copyright © 2015 Jon Hjelle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JAHJRTC : NSObject
+
+@end
---
+++ JAHJRTC/JAHJRTC.m
@@ -0,0 +1,11 @@
+//
+//  JAHJRTC.m
+//
+//  Copyright © 2015 Jon Hjelle. All rights reserved.
+//
+
+#import "JAHJRTC.h"
+
+@implementation JAHJRTC
+
+@end
